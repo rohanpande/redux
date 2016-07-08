d23efe9c1f1ff67edbc15026c4e9e22cdcb4517b
Rohan Pande <rohanpande@sanisoft.com>
2016-07-08 16:40:34 +0530

    added more changes

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -1,10 +1,27 @@
 'use strict';
 
-import React from 'react';
+import React, { Component } from 'react';
 
-class SearchBar extends React.Component {
-  redner() {
-    return <input />;
+class SearchBar extends Component {
+  constructor(props) {
+      super(props);
+      this.state = { term:'' };
+  }
+
+  render() {
+    return (
+      <div>
+        <input
+          value={this.state.term}
+          onChange={event => this.setState({ term: event.target.value })}
+        />
+      </div>
+    );
+
+  }
+
+  onInputChange(event) {
+    console.log(event.target.value);
   }
 }
 
--- src/index.js
+++ src/index.js
@@ -1,19 +1,32 @@
-// create a new component. This comopnent should produce some HTML
-
-// Take this component's generated HTML and put it on the page in the DOM.
-
-import React from 'react';
-import ReactDOM from 'react-dom';
-import SearchBar from './components/search_bar';
-
+import React, {Component} from 'react';
+import ReactDOM from 'react-dom'; // load from node_modules
+import YTSearch from 'youtube-api-search';
+import SearchBar from './components/search_bar'; // load from file.
+import VideoList from './components/video_list';
+import VideoDetail from './components/video_detail';
 const API_KEY = 'AIzaSyCF9wVMXUrfqtAOTyq99lJSEXBV8Q-PY0c';
 
-const App = () => {
+
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = { videos:[] };
+
+    YTSearch({ key:API_KEY, term: 'surfboards' }, (videos) => {
+      this.setState({ videos });
+    });
+  }
+  render() {
     return (
       <div>
         <SearchBar />
+        <VideoDetail video={this.state.videos[0]} />
+        <VideoList videos={this.state.videos} />
       </div>
     );
-};
+  };
+}
 
 ReactDOM.render(<App />,document.querySelector('.container'));
